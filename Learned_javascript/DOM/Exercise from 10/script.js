238a4a68e5bf52eb478d2efd0d46f04c6c149c9c
shacz007 <sachinsiddrampur@gmail.com>
2023-02-13 17:59:10 +0530

    sss

---
+++ Learned_javascript/DOM/Exercise from 10/script.js
@@ -0,0 +1,11 @@
+function saySomething(whatToSay) {
+    window.alert(whatToSay);
+  }
+  
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+
+  saySomething("hi");
+  const result = add(3, 5);
+window.alert(result);
