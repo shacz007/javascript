171816f1d8d8c58de024ce173e14e9686617168b
shacz007 <sachinsiddrampur@gmail.com>
2023-02-24 16:56:23 +0530

    hi

---
+++ practice_assgn_javascript/golf.js
@@ -0,0 +1,31 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if(strokes == 1 ){
+  return "Hole-in-one!";
+}
+else if(strokes <= par - 2){
+  return "Eagle"
+}
+else if(strokes == par - 1){
+  return "Birdie"
+}
+else if(strokes == par  ){
+    return "par"
+}
+else if(strokes == par +1){
+  return "Bogey"
+}
+else if(strokes == par +2){
+    return "Double Bogey"
+}
+else{
+  return "Go Home!"
+}
+
+  // return "Change Me";
+  // Only change code above this line
+}
+
+console.log(golfScore(1,1)); 
