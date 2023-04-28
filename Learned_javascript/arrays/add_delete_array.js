74d88560e31147831d1437b85099306365876045
shacz007 <sachinsiddrampur@gmail.com>
2023-04-28 22:51:15 +0530

    oj

--- Learned_javascript/arrays/add_delete_array.js
+++ Learned_javascript/arrays/add_delete_array.js
@@ -7,7 +7,7 @@ let b=[1,2,3,4,5,6,7,8,9]
 b.shift();
 delete b[3];//delete keyword can also be used to deleteelements from array
 console.log(b);
-//.shift() deletes the first element of the array
+//.shift() deletes the first element of the array.
 
 b.unshift(5);
 console.log(b);
